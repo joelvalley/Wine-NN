edbd7f2f1d8ea31842a44c81753619ac6e36314e
Joel <joeldpv6@icloud.com>
2025-06-26 00:16:16 -0400

    Add final predictions, PCA & plot, add images

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from pathlib import Path
 
 # Set the hyperparameters for the data
 NUM_CLASSES = 3
 NUM_FEATURES = 13
 RANDOM_SEED = 27
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.001
 
 # Import the dataset from sklearn
 data = load_wine()
@@ -29,29 +31,32 @@ print(f"Using device: {device}")
 
 # Create the model
 class WineModel(nn.Module):
-    def __init__(self, input_features: int, output_features: int, hidden_units=50):
+    def __init__(self, input_features: int, output_features: int, hidden_units=32):
         """Initializes multi-class classification model.
         
         Args:
             input_features (int): Num of input features to the model
             output_features (int): Num of output features (number of output classes)
-            hidden_units (int): Num of hidden units between layers, default 50
+            hidden_units (int): Num of hidden units between layers, default 32
         """
         super().__init__()
         self.layer_1 = nn.Linear(in_features=input_features, out_features=hidden_units)   # Input layer
         self.layer_2 = nn.Linear(in_features=hidden_units, out_features=hidden_units)   # Hidden layer
-        self.layer_3 = nn.Linear(in_features=hidden_units, out_features=output_features)    # Output layer
+        self.layer_3 = nn.Linear(in_features=hidden_units, out_features=hidden_units)   # Hidden layer
+        self.layer_4 = nn.Linear(in_features=hidden_units, out_features=output_features)    # Output layer
         self.relu = nn.ReLU()
 
     def forward(self, x):   # x -> layer_1 -> layer_2 -> layer_3 -> output
         x = self.relu(self.layer_1(x))
         x = self.relu(self.layer_2(x))
-        x = self.layer_3(x)
+        x = self.relu(self.layer_3(x))
+        x = self.layer_4(x)
         return x
     
 # Create model
 model = WineModel(input_features=13,
-                  output_features=3).to(device)
+                  output_features=3, 
+                  hidden_units=64).to(device)
 
 # Setup loss fn & optimizer
 loss_fn = nn.CrossEntropyLoss()
@@ -71,7 +76,7 @@ torch.mps.manual_seed(27)
 np.random.seed(27)
 
 # Set the number of epochs
-epochs = 1001
+epochs = 1501
 
 # Put the data on the target device
 X_train, y_train = X_train.to(device), y_train.to(device)
@@ -83,7 +88,7 @@ for epoch in range(epochs):
 
     # 1. Forward pass
     y_logits = model(X_train)
-    y_pred = torch.argmax(torch.softmax(y_logits, dim=1), dim=1)
+    y_pred = torch.argmax(torch.softmax(y_logits, dim=1), dim=1)    # Logits -> pred probs -> preds
 
     #2. Calculate loss
     loss = loss_fn(y_logits, y_train)
@@ -113,3 +118,65 @@ for epoch in range(epochs):
         # 3. Print out what's happenin'
         if epoch % 100 == 0:
             print(f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}")
+
+# Make predictions
+model.eval()
+with torch.inference_mode():
+    # 1. Forward pass
+    logits = model(X_test)
+    pred = torch.argmax(torch.softmax(logits, dim=1), dim=1)
+
+# Save the model
+
+# 1. Create model's directory
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+# 2. Create model save path
+MODEL_NAME = "wine_model_0.pt"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# 3. Save the model's state dict
+print(f"Saving the model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model.state_dict(), 
+           f=MODEL_SAVE_PATH)
+
+# Function to convert the data using PCA and plot
+def pca_and_plot_fn(model: nn.Module, 
+                    X: torch.Tensor,
+                    y_true: torch.Tensor, 
+                    y_preds: torch.Tensor, 
+                    title_1: str, 
+                    title_2: str):
+    """Performs principled component analysis on the datasets and plots the results"""
+
+    # Put data on the cpu
+    model.to("cpu")
+    X, y_true, y_preds = X.to("cpu"), y_true.to("cpu"), y_preds.to("cpu")
+
+    # Transform the data with PCA
+    pca = PCA(n_components=2)
+    X_pca = pca.fit_transform(X)
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 5))
+
+    scatter_1 = ax[0].scatter(x=X_pca[:, 0], y=X_pca[:, 1], c=y_true, cmap="RdYlBu", edgecolors="k")
+    ax[0].set_xlabel("PC1")
+    ax[0].set_ylabel("PC2")
+    ax[0].set_title(title_1)
+
+    handles_1, _ = scatter_1.legend_elements()
+    ax[0].legend(handles_1, ["Class 1", "Class 2", "Class 3"], loc="upper right")
+
+    scatter_2 = ax[1].scatter(x=X_pca[:, 0], y=X_pca[:, 1], c=y_preds, cmap="RdYlBu", edgecolors="k")
+    ax[1].set_xlabel("PC1")
+    ax[1].set_ylabel("PC2")
+    ax[1].set_title(title_2)
+
+    handles_2, _ = scatter_2.legend_elements()
+    ax[1].legend(handles_2, ["Class 1", "Class 2", "Class 3"], loc="upper right")
+
+    plt.show()
+
+# Plot the data
+pca_and_plot_fn(model, X_test, y_test, pred, "PCA of Wine Test Dataset", "PCA of Model's Test Predictions")
